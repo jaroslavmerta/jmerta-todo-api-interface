98d5d6e6b445158bbf50c5c8570c08dc16de3110
Jaroslav Merta <81035041+jaroslavmerta@users.noreply.github.com>
2022-08-07 21:04:46 +0200

    tslint

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
 interface Task {
-    id: string
-    content: string
-    finished: string
-  }
-  
-interface GetTaskResponse {
-data: Task[]
+  id: string;
+  content: string;
+  finished: string;
 }
 
-export {GetTaskResponse};
+interface GetTaskResponse {
+  data: Task[];
+}
+
+export { GetTaskResponse };
